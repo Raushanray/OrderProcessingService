fda53a6857f2c07d9c266b5d7f4fab8093d1dfff
Raushanray <raushankuray1998@gmail.com>
2023-06-13 17:00:12 +0530

    Create  Test a negative scenario for testing..

--- test/com/lcwr/order/bo/OrderBOImplTest.java
+++ test/com/lcwr/order/bo/OrderBOImplTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
 
@@ -18,17 +19,18 @@ public class OrderBOImplTest {
 
 	@Mock
 	OrderDAO dao;
+	private OrderBOImpl bo;
 	
 	@Before
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
+		bo = new OrderBOImpl();
+		bo.setDao(dao);
 	}
 	
 	@Test
 	public void placeOrder_Should_Create_An_Order() throws SQLException, BOException {
 		
-		OrderBOImpl bo = new OrderBOImpl();
-		bo.setDao(dao);
 
 		Order order = new Order();
 		when(dao.create(order)).thenReturn(new Integer(1));
@@ -39,6 +41,17 @@ public class OrderBOImplTest {
 		verify(dao).create(order);
 	}
 	
-	
+	@Test
+	public void placeOrder_Should_Not_Create_An_Order() throws SQLException, BOException {
+		
+
+		Order order = new Order();
+		when(dao.create(order)).thenReturn(new Integer(0));
+		
+		boolean result = bo.placeOrder(order);
+		
+		assertFalse(result);
+		verify(dao).create(order);
+	}
 
 }
