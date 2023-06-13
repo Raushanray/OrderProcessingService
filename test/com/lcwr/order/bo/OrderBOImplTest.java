32e3e28799b6ce081856a12051775d6f1c464045
Raushanray <raushankuray1998@gmail.com>
2023-06-13 14:25:14 +0530

    Run this test class using JUnitTest and  Verify the results...

--- test/com/lcwr/order/bo/OrderBOImplTest.java
+++ test/com/lcwr/order/bo/OrderBOImplTest.java
@@ -4,14 +4,17 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
 
 import java.sql.SQLException;
 
+import com.lcwr.order.bo.exception.BOException;
 import com.lcwr.order.dao.OrderDAO;
 import com.lcwr.order.dto.Order;
 
-class OrderBOImplTest {
+public class OrderBOImplTest {
 
 	@Mock
 	OrderDAO dao;
@@ -22,7 +25,7 @@ class OrderBOImplTest {
 	}
 	
 	@Test
-	public void placeOrder_Should_Create_An_Order() throws SQLException {
+	public void placeOrder_Should_Create_An_Order() throws SQLException, BOException {
 		
 		OrderBOImpl bo = new OrderBOImpl();
 		bo.setDao(dao);
@@ -30,6 +33,10 @@ class OrderBOImplTest {
 		Order order = new Order();
 		when(dao.create(order)).thenReturn(new Integer(1));
 		
+		boolean result = bo.placeOrder(order);
+		
+		assertTrue(result);
+		verify(dao).create(order);
 	}
 	
 	
